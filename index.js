a9cf981e581fbb153352b4ffde17a2fd1b3eb966
aaaasif <alexasif4@gmail.com>
2021-10-31 11:37:37 +0600

    js added

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function run() {
         await client.connect();
         const database = client.db('TOURISO');
         const servicesCollection = database.collection('services');
+        const servicesDetails = database.collection('details');
 
         // GET API
         app.get('/services', async (req, res) => {
@@ -28,15 +29,28 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         });
+        app.get('/details', async (req, res) => {
+            const cursor = servicesDetails.find({});
+            const details = await cursor.toArray();
+            res.send(details);
+        });
 
         // GET Single Service
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            console.log('getting specific service', id);
+            console.log('getting specific service id', id);
             const query = { _id: ObjectId(id) };
             const service = await servicesCollection.findOne(query);
             res.json(service);
         })
+        app.get('/details/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific details', id);
+            const query = { _id: ObjectId(id) };
+            const details = await servicesDetails.findOne(query);
+            res.json(details);
+        })
+
 
         // POST API
         app.post('/services', async (req, res) => {
@@ -44,6 +58,14 @@ async function run() {
             console.log('hit the post api', service);
 
             const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        });
+        app.post('/details', async (req, res) => {
+            const detail = req.body;
+            console.log('hit the post api', detail);
+
+            const result = await servicesDetails.insertOne(detail);
             console.log(result);
             res.json(result)
         });
@@ -55,6 +77,13 @@ async function run() {
             const result = await servicesCollection.deleteOne(query);
             res.json(result);
         })
+        // load cart data according to user id get api
+        app.get("/details/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { uid: id };
+            const result = await servicesDetails.find(query).toArray();
+            res.json(result);
+        });
 
     }
     finally {
@@ -65,7 +94,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Running Genius Server');
+    res.send('Running TOURISO Server');
 });
 
 app.get('/hello', (req, res) => {
@@ -73,5 +102,5 @@ app.get('/hello', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Running Genius Server on port', port);
+    console.log('Running TOURISO Server on port', port);
 })
