d529e5867c4ddc9c4f175bb748299fe6df889859
aaaasif <alexasif4@gmail.com>
2021-10-31 19:00:10 +0600

    js added

--- index.js
+++ index.js
@@ -71,10 +71,10 @@ async function run() {
         });
 
         // DELETE API
-        app.delete('/services/:id', async (req, res) => {
+        app.delete('/details/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const result = await servicesCollection.deleteOne(query);
+            const result = await servicesDetails.deleteOne(query);
             res.json(result);
         })
         // load cart data according to user id get api
